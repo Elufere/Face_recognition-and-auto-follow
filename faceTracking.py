5e485635dff4ae8edf81f8892ef55bc5aad15c9b
Elufere Innocent <122732911+Elufere@users.noreply.github.com>
2023-04-02 14:13:45 +0100

    Update faceTracking.py

--- faceTracking.py
+++ faceTracking.py
@@ -19,9 +19,6 @@ for image in images:
 
 print(len(encodingList))
 
-# vid = cv2.VideoCapture(0)
-
-
 me = tello.Tello()
 me.connect()
 print(me.get_battery())
@@ -35,41 +32,17 @@ fbRange = [2000, 5000]
 pid = [0.2, 0.2, 0]
 pError = 0
 
-# Load the image of the face to track and encode it
-# face_to_track = face_recognition.load_image_file('venv/Resources/Images/Dre.jpg')
-# face_to_track_encodings = face_recognition.face_encodings(face_to_track)[0]
-
-
-
 
 def findEncodedFace(img):
-    # small_frame = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
-    # rgb_small_frame = small_frame[:, :, ::-1]
     face_locations = face_recognition.face_locations(img)
     # Encode each detected face and compare it with the reference face
     print("faces found: ", len(face_locations))
     for face_location in face_locations:
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
         top, right, bottom, left = face_location
-        #
-        # top *= 4
-        # right *= 4
-        # bottom *= 4
-        # left *= 4
 
         face_encodings = face_recognition.face_encodings(img, [(top, right, bottom, left)])
-        # print(len(face_encodings[0]))
-        # Compare the face encoding with the reference face encoding
 
-        # img_ = cv2.rectangle(img, (left, top), (right, bottom), (255, 0, 0), 2)
-        # img_ = cv2.cvtColor(img, cv2.BGR2RGB)
-
-
-        # w_ = right - left
-        # h_ = bottom - top
-        # cx = left + w_ // 2
-        # cy = top + h_ // 2
-        # return img_, [[cx, cy], w_ * h_]
         if len(face_encodings) > 0 and any(face_recognition.compare_faces(encodingList, face_encodings[0])):
             img_ = cv2.rectangle(img, (left, top), (right, bottom), (255, 0, 0), 2)
             w_ = right - left
@@ -81,7 +54,6 @@ def findEncodedFace(img):
 
 
 def trackEncodedFace(img, info, w, pid, pError):
-    # print(info)
 
     x = info[0][0]
     y = info[0][1]
@@ -121,7 +93,6 @@ def trackEncodedFace(img, info, w, pid, pError):
 
 while True:
     img = me.get_frame_read().frame
-    # ret, img = vid.read()
     img = cv2.resize(img, (w, h))
     img, info = findEncodedFace(img)
     pError = trackEncodedFace(img, info, w, pid, pError)
